ff9049696c8a9c6a0be37deb30783c3d5c21fea1
shivank55 <shivankjain555@gmail.com>
2024-07-16 12:15:58 +0530

    file handling: LEARNING copy

--- file.js
+++ file.js
@@ -34,4 +34,6 @@ const fs = require("fs");
 
 // fs.appendFileSync("./contacts.txt", new Date().getDate().toLocaleString())  /// always string
 
-fs.appendFileSync("./contacts.txt", `\n James Jain : +1893939399`)
+fs.appendFileSync("./contacts.txt", `\n ${Date.now()} James Jain : +1893939399`)
+
+fs.cpSync("./contacts.txt","./copy.txt") //copy
