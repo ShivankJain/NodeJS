695ded43be50691e7d783ee668a8f9ae95867b46
shivank55 <shivankjain555@gmail.com>
2024-07-16 12:06:10 +0530

    file handling: write/create and read done

--- file.js
+++ file.js
@@ -1,5 +1,35 @@
 const fs = require("fs");
 
+// ############### CREATE/WRITE #################
+
 // Sync... Call
 
-fs.writeFileSync("./test.txt", "Hey There !!!");
+// fs.writeFileSync("./test.txt", "Hey There !!!");
+
+// Async
+
+// fs.writeFile("./test.txt","Hello World", (err)=>{})
+// Synchronous and Asynchronous ... Blocking and Non Blocking requests
+
+ //############    READ   ########
+
+// >>>>>>>Synchronus returns
+// const res=fs.readFileSync("./contacts.txt", "utf-8")
+// console.log(res);
+
+
+// >>>>>>>Asyncronous Does not returns and expects a callback function
+
+// fs.readFile("./contacts.txt","utf-8", (err,res)=>{
+//     if(err){ 
+//         console.log('Erro : ',err)
+//     }   
+//     else{
+//         console.log(res)
+//     }
+// })
+
+
+// ########################### APPEND #######################
+
+
